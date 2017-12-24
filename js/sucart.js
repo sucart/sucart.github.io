91353fe5fb8abda25ec07a51e843a400315f447d
Pablo Abad <pabadrubio@gmail.com>
2017-12-24 13:08:44 +0100

    Changed effects for touch devices

--- js/sucart.js
+++ js/sucart.js
@@ -13,4 +13,25 @@ $(document).ready(function() {
       tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
     }
   });
+
+  if (is_touch_device()) {
+    window.sr = ScrollReveal({ reset: true });
+    sr.reveal('.showreelframe', {
+      duration: 2000,
+      delay: 200,
+      scale: 1,
+      distance: '0px',
+      viewFactor: 0.5
+    }, 0);
+
+    document.documentElement.classList.add('can-touch')
+  } else {
+    document.documentElement.classList.add('can-hover')
+  }
+
 });
+
+function is_touch_device() {
+  return 'ontouchstart' in window        // works on most browsers
+      || navigator.maxTouchPoints;       // works on IE10/11 and Surface
+};
